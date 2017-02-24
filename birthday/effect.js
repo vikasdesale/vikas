f27e589c24282637b26ba676d43bae8e41b6ddda
vikasdesale <vikas.desaleu@gmail.com>
2017-02-24 17:46:30 +0530

    location_redirect

--- birthday/effect.js
+++ birthday/effect.js
@@ -155,34 +155,9 @@ $('document').ready(function(){
 	});
 	
 	$('#story').click(function(){
-		$(this).fadeOut('slow');
-		$('.cake').fadeOut('fast').promise().done(function(){
-			$('.message').fadeIn('slow');
-		});
-		
-		var i;
-
-		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
-			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
-					$('.cake').fadeIn('fast');
-				});
-				
-			}
-			else{
-				msgLoop(i);
-			}			
-
-		});
-			// body...
-		}
-		
-		msgLoop(0);
-		
-	});
+	
+	var delay = 1000; //Your delay in milliseconds
+setTimeout(function(){ window.location = https://www.w3schools.com/jsref/prop_style_display.asp; }, delay);
 });
 
 

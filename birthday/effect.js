0a92da4fce0e3102a7b79cb5fded4bd00ce593ea
vikasdesale <vikas.desaleu@gmail.com>
2017-02-24 17:04:40 +0530

    location_change9

--- birthday/effect.js
+++ birthday/effect.js
@@ -160,6 +160,29 @@ $('document').ready(function(){
 			$('.message').fadeIn('slow');
 		});
 		
+		var i;
+
+		function msgLoop (i) {
+			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
+			i=i+1;
+			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
+			if(i==50){
+				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+					$('.cake').fadeIn('fast');
+				});
+				
+			}
+			else{
+				msgLoop(i);
+			}			
+
+		});
+			// body...
+		}
+		
+		msgLoop(0);
+		
+	});
 });
 
 

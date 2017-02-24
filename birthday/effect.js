d55a10001426347180b378a5628be39c01628267
vikasdesale <vikas.desaleu@gmail.com>
2017-02-24 19:14:56 +0530

    card14325

--- birthday/effect.js
+++ birthday/effect.js
@@ -156,13 +156,29 @@ $('document').ready(function(){
 	
 	$('#story').click(function(){
 		$(this).fadeOut('slow');
-		$('.birthday').fadeOut('fast').promise().done(function(){
-			$('.cards').fadeIn('slow');
+		$('.birthday').fadeOut('slow');
+		$('.cards').fadeIn('slow');
+		$('.cake').fadeOut('fast').promise().done(function(){
+			$('.message').fadeIn('slow');
 		});
 		
 		var i;
 
 		function msgLoop (i) {
+			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
+			i=i+1;
+			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
+			if(i==50){
+				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+					$('.cake').fadeIn('fast');
+				});
+				
+			}
+			else{
+				msgLoop(i);
+			}			
+
+		});
 			// body...
 		}
 		

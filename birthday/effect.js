c207426e45f5fec1ad6526d03502e1c01c5a4627
vikasdesale <vikas.desaleu@gmail.com>
2017-02-24 16:41:23 +0530

    location_change2

--- birthday/effect.js
+++ birthday/effect.js
@@ -156,33 +156,9 @@ $('document').ready(function(){
 	
 	$('#story').click(function(){
 		$(this).fadeOut('slow');
-		$('.cake').fadeOut('fast').promise().done(function(){
-			$('.message').fadeIn('slow');
-		});
-		
-		var i;
+		    window.location.href = "http://www.google.com";
 
-		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
-			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
-					$('.cake').fadeIn('fast');
-				});
-				
-			}
-			else{
-				msgLoop(i);
-			}			
-
-		});
-			// body...
-		}
 		
-		msgLoop(0);
-		
-	});
 });
 
 
